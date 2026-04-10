7911dc8d7ea4c3646b4189d6212f18ab4fa37717
ForeverGreenDam <2985601245@qq.com>
2026-04-11 00:55:39 +0800

    feat(config): update application.yml and pom.xml for Redis and MySQL integration

--- src/test/java/com/greendam/ai/RunAppTests.java
+++ src/test/java/com/greendam/ai/RunAppTests.java
@@ -1,13 +1,20 @@
 package com.greendam.ai;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 @SpringBootTest
 class RunAppTests {
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
 
     @Test
     void contextLoads() {
+        stringRedisTemplate.opsForValue().set("test1", "world");
+        String value = stringRedisTemplate.opsForValue().get("test1");
+        System.out.println("Value for 'test1': " + value);
     }
 
 }
